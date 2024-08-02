0d6bed4e13cc9edd4bd9e4e7b3dd6748aae88b9d
Yuriy Shpak <yuriy.shpak@jetcoders.com>
2024-08-02 18:03:02 +0300

    Update index.js

--- index.js
+++ index.js
@@ -1,16 +1,17 @@
 try {
-	process.env.LESSONS = process.env.LESSONS ?? 1;
-	process.env.TIMER = process.env.TIMER ?? 1;
+	const LESSONS = process.env.LESSONS ?? 1;
+	const TIMER = process.env.TIMER ?? 1;
+	const JWT = process.env.DUOLINGO_JWT;
 
 	const headers = {
 		"Content-Type": "application/json",
-		Authorization: `Bearer ${process.env.DUOLINGO_JWT}`,
+		Authorization: `Bearer ${JWT}`,
 		"user-agent":
 			"Mozilla/5.0 (Macintosh; Intel Mac OS X 10_15_7) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/121.0.0.0 Safari/537.36",
 	};
 
 	const { sub } = JSON.parse(
-		Buffer.from(process.env.DUOLINGO_JWT.split(".")[1], "base64").toString(),
+		Buffer.from(JWT.split(".")[1], "base64").toString(),
 	);
 
 	const { fromLanguage, learningLanguage } = await fetch(
@@ -20,111 +21,17 @@ try {
 		},
 	).then((response) => response.json());
 
-	console.log(`🎉 Start! Lessons = ${process.env.LESSONS}, Timer = ${process.env.TIMER}`);
+	console.log(`🎉 Start! Lessons = ${LESSONS}, Timer = ${TIMER}`);
 
-	await start(process.env.LESSONS, process.env.TIMER, 0, fromLanguage, learningLanguage, headers);
+	await start(LESSONS, TIMER, 0, fromLanguage, learningLanguage, headers);
 
-
-
-	// for (let i = 0; i < process.env.LESSONS; i++) {
-	// 	const session = await fetch(
-	// 		"https://www.duolingo.com/2017-06-30/sessions",
-	// 		{
-	// 			body: JSON.stringify({
-	// 				challengeTypes: [
-	// 					"assist",
-	// 					"characterIntro",
-	// 					"characterMatch",
-	// 					"characterPuzzle",
-	// 					"characterSelect",
-	// 					"characterTrace",
-	// 					"characterWrite",
-	// 					"completeReverseTranslation",
-	// 					"definition",
-	// 					"dialogue",
-	// 					"extendedMatch",
-	// 					"extendedListenMatch",
-	// 					"form",
-	// 					"freeResponse",
-	// 					"gapFill",
-	// 					"judge",
-	// 					"listen",
-	// 					"listenComplete",
-	// 					"listenMatch",
-	// 					"match",
-	// 					"name",
-	// 					"listenComprehension",
-	// 					"listenIsolation",
-	// 					"listenSpeak",
-	// 					"listenTap",
-	// 					"orderTapComplete",
-	// 					"partialListen",
-	// 					"partialReverseTranslate",
-	// 					"patternTapComplete",
-	// 					"radioBinary",
-	// 					"radioImageSelect",
-	// 					"radioListenMatch",
-	// 					"radioListenRecognize",
-	// 					"radioSelect",
-	// 					"readComprehension",
-	// 					"reverseAssist",
-	// 					"sameDifferent",
-	// 					"select",
-	// 					"selectPronunciation",
-	// 					"selectTranscription",
-	// 					"svgPuzzle",
-	// 					"syllableTap",
-	// 					"syllableListenTap",
-	// 					"speak",
-	// 					"tapCloze",
-	// 					"tapClozeTable",
-	// 					"tapComplete",
-	// 					"tapCompleteTable",
-	// 					"tapDescribe",
-	// 					"translate",
-	// 					"transliterate",
-	// 					"transliterationAssist",
-	// 					"typeCloze",
-	// 					"typeClozeTable",
-	// 					"typeComplete",
-	// 					"typeCompleteTable",
-	// 					"writeComprehension",
-	// 				],
-	// 				fromLanguage,
-	// 				isFinalLevel: false,
-	// 				isV2: true,
-	// 				juicy: true,
-	// 				learningLanguage,
-	// 				smartTipsVersion: 2,
-	// 				type: "GLOBAL_PRACTICE",
-	// 			}),
-	// 			headers,
-	// 			method: "POST",
-	// 		},
-	// 	).then((response) => response.json());
-
-	// 	const response = await fetch(
-	// 		`https://www.duolingo.com/2017-06-30/sessions/${session.id}`,
-	// 		{
-	// 			body: JSON.stringify({
-	// 				...session,
-	// 				heartsLeft: 0,
-	// 				startTime: (+new Date() - 60000) / 1000,
-	// 				enableBonusPoints: false,
-	// 				endTime: +new Date() / 1000,
-	// 				failed: false,
-	// 				maxInLessonStreak: 9,
-	// 				shouldLearnThings: true,
-	// 			}),
-	// 			headers,
-	// 			method: "PUT",
-	// 		},
-	// 	).then((response) => response.json());
-
-	// 	xp += response.xpGain;
-	// }
 } catch (error) {
-	console.log("❌ Something went wrong");
+	console.log("❌ Something went wrong", error);
+
+	for(let key of error){
+		console.log(`${key}: ${error[key]}`);
+	}
+	
 	if (error instanceof Error) {
 		console.log(error.message);
 	}
@@ -227,7 +134,7 @@ async function start(lessons, timer, xp, fromLanguage, learningLanguage, headers
 
 	xp += response.xpGain;
 
-	console.log(`🎉 You won ${xp} XP`, new Date().toString());
+	console.log(`🎉 You won ${xp} XP`, new Date().toLocaleTimeString());
 
 	lessons--;
 
@@ -238,4 +145,14 @@ async function start(lessons, timer, xp, fromLanguage, learningLanguage, headers
 	}
 
 	return xp;
+} catch (error) {
+	console.log("❌ Total ERROR: ", error);
+
+	for(let key of error){
+		console.log(`${key}: ${error[key]}`);
+	}
+
+	if (error instanceof Error) {
+		console.log(error.message);
+	}
 }
